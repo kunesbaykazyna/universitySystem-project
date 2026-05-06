6fd42e2439c2068235b8495955cc5bdb56772470
Aidarkyzy Dinara <dinaraajdarkyzy@gmail.com>
2026-05-06 17:26:26 +0500

    Add files via upload

---
+++ src/factory/UserFactory.java
@@ -0,0 +1,58 @@
+package factory;
+
+import models.*;
+import enumerations.Faculty;
+import enumerations.GraduateLevel;
+import enumerations.TeacherType;
+
+public class UserFactory {
+
+    private UserFactory() {}
+
+    public static User createUser(String role,
+                                  String userId, String name,
+                                  String password, String login,
+                                  Object... extraArgs) {
+        return switch (role.toUpperCase()) {
+
+            case "STUDENT" -> {
+                Faculty faculty      = (Faculty) extraArgs[0];
+                int     yearsOfStudy = (int)     extraArgs[1];
+                yield new Student(userId, name, password, login, yearsOfStudy, faculty);
+            }
+
+            case "TEACHER" -> {
+                Faculty     faculty = (Faculty)     extraArgs[0];
+                TeacherType type    = (TeacherType) extraArgs[1];
+                double      salary  = (double)      extraArgs[2];
+                yield new Teacher(userId, name, password, login, salary, faculty, type);
+            }
+
+            case "ADMIN" -> {
+                double salary = (double) extraArgs[0];
+                yield new Admin(userId, name, password, login, salary);
+            }
+
+            case "MANAGER" -> {
+                double salary = (double) extraArgs[0];
+                yield new Manager(userId, name, password, login, salary);
+            }
+
+            case "TECH_SUPPORT" -> {
+                double salary = (double) extraArgs[0];
+                yield new TechSupportSpecialist(userId, name, password, login, salary);
+            }
+
+            case "GRADUATED_STUDENT" -> {
+                Faculty       faculty      = (Faculty)       extraArgs[0];
+                int           yearsOfStudy = (int)           extraArgs[1];
+                GraduateLevel level        = (GraduateLevel) extraArgs[2];
+                Teacher       supervisor   = (Teacher)       extraArgs[3];
+                yield new GraduatedStudent(userId, name, password, login,
+                        yearsOfStudy, faculty, supervisor, level);
+            }
+
+            default -> throw new IllegalArgumentException("Белгісіз рол: " + role);
+        };
+    }
+}
